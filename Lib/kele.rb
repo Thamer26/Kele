9591ae1fa810973092b9605398a45f850a15e761
Thamer26 <thamer.alhejailan1@gmail.com>
2017-11-06 14:37:24 +0400

    Add files via upload

---
+++ Lib/kele.rb
@@ -0,0 +1,60 @@
+require "kele/version"
+require "kele/errors"
+require "httparty"
+
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
+    raise InvalidStudentCodeError.new() if response.code == 401
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+    @user_data.keys.each do |key|
+      self.class.send(:define_method, key.to_sym) do
+        @user_data[key]
+      end
+    end
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
+  end
+
+  def get_roadmap(roadmap_id)
+    response = self.class.get(base_api_endpoint("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(base_api_endpoint("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)
+  end
+
+  def get_messages(page)
+    response = self.class.get(base_api_endpoint("message_threads?page=#{page}"), headers: { "authorization" => @auth_token })
+    @get_messages = JSON.parse(response.body)
+  end
+
+  def create_message(recipient_id, subject, message)
+    response = self.class.post(base_api_endpoint("messages"), body: { "user_id": id, "recipient_id": recipient_id, "subject": subject, "stripped-text": message }, headers: { "authorization" => @auth_token })
+    puts response
+  end
+
+  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(base_api_endpoint("checkpoint_submissions"), body: { "checkpoint_id": checkpoint_id, "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "comment": comment }, headers: { "authorization" => @auth_token })
+    puts response
+  end
+
+private
+
+  def base_api_endpoint(end_point)
+    "https://www.bloc.io/api/v1/#{end_point}"
+  end
+
+end
